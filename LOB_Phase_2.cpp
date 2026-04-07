488205da59bb0374929a429d14df6864d1a5c663
PranavMundada <pjmundada2005@gmail.com>
2026-04-07 20:01:20 +0530

    Phase 2 completed with Order Cancellation Feature

--- LOB_Phase_2.cpp
+++ LOB_Phase_2.cpp
@@ -32,7 +32,8 @@ struct OrderNode
     uint64_t quantity;
     Side side;
     uint64_t timestamp;
-    int32_t nextOrderIndex;
+    int32_t nextOrderIndex = NULL_INDEX;
+    int32_t prevOrderIndex = NULL_INDEX;
 };
 
 struct PriceLevel
@@ -46,11 +47,10 @@ class OrderBook
 
 private:
     std::vector<OrderNode> orderPool;
+    std::vector<PriceLevel> asks, bids;
+    std::vector<int32_t> orderMap;
 
     int32_t firstFreeIndex = 0;
-
-    std::vector<PriceLevel> asks, bids;
-
     int32_t bestBidIndex = 0, bestAskIndex = 0;
 
     int32_t allocateOrder()
@@ -75,6 +75,7 @@ public:
     OrderBook(size_t maxOrders)
     {
         orderPool.resize(maxOrders);
+        orderMap.resize(maxOrders, NULL_INDEX);
 
         for (size_t i = 0; i < maxOrders - 1; i++)
         {
@@ -88,6 +89,8 @@ public:
         int32_t newIndex = allocateOrder();
         orderPool[newIndex] = {incomingOrder.orderId, incomingOrder.price, incomingOrder.quantity, incomingOrder.side, incomingOrder.timestamp};
         orderPool[newIndex].nextOrderIndex = NULL_INDEX;
+        orderPool[newIndex].prevOrderIndex = NULL_INDEX;
+        orderMap[incomingOrder.orderId] = newIndex;
 
         if (orderPool[newIndex].side == Side::Buy)
         {
@@ -105,6 +108,7 @@ public:
                 {
                     int32_t indexToFree = asks[bestAskIndex].headOrderIndex;
                     asks[bestAskIndex].headOrderIndex = orderPool[oldestIndex].nextOrderIndex;
+                    orderMap[orderPool[oldestIndex].orderId] = NULL_INDEX;
                     deallocateOrder(indexToFree);
                 }
                 if (asks[bestAskIndex].headOrderIndex == NULL_INDEX)
@@ -127,6 +131,7 @@ public:
                 {
                     int32_t indexToFree = bids[bestBidIndex].headOrderIndex;
                     bids[bestBidIndex].headOrderIndex = orderPool[oldestIndex].nextOrderIndex;
+                    orderMap[orderPool[oldestIndex].orderId] = NULL_INDEX;
                     deallocateOrder(indexToFree);
                 }
                 if (bids[bestBidIndex].headOrderIndex == NULL_INDEX)
@@ -153,6 +158,7 @@ public:
                     else
                     {
                         orderPool[it->tailOrderIndex].nextOrderIndex = newIndex;
+                        orderPool[newIndex].prevOrderIndex = it->tailOrderIndex;
                         it->tailOrderIndex = newIndex;
                     }
                 }
@@ -185,6 +191,7 @@ public:
                     else
                     {
                         orderPool[it->tailOrderIndex].nextOrderIndex = newIndex;
+                        orderPool[newIndex].prevOrderIndex = it->tailOrderIndex;
                         it->tailOrderIndex = newIndex;
                     }
                 }
@@ -203,8 +210,65 @@ public:
         }
         else
         {
+            orderMap[incomingOrder.orderId] = NULL_INDEX;
             deallocateOrder(newIndex);
         }
+    }
+
+    void cancelOrder(uint64_t orderId)
+    {
+
+        if (orderId >= orderMap.size())
+            return;
+        int32_t targetIndex = orderMap[orderId];
+        if (targetIndex == NULL_INDEX)
+            return;
+
+        OrderNode &targetNode = orderPool[targetIndex];
+        int32_t prevIndex = targetNode.prevOrderIndex;
+        int32_t nextIndex = targetNode.nextOrderIndex;
+
+        std::vector<PriceLevel>::iterator it;
+        if (targetNode.side == Side::Buy)
+        {
+            it = std::lower_bound(bids.begin(), bids.end(), targetNode.price, [](const PriceLevel &level, uint64_t price)
+                                  { return level.price > price; });
+        }
+        else
+        {
+            it = std::lower_bound(asks.begin(), asks.end(), targetNode.price, [](const PriceLevel &level, uint64_t price)
+                                  { return level.price < price; });
+        }
+
+        if (prevIndex == NULL_INDEX)
+        {
+            if (nextIndex == NULL_INDEX)
+            {
+                it->headOrderIndex = NULL_INDEX;
+                it->tailOrderIndex = NULL_INDEX;
+            }
+            else
+            {
+                it->headOrderIndex = nextIndex;
+                orderPool[nextIndex].prevOrderIndex = NULL_INDEX;
+            }
+        }
+        else
+        {
+            if (nextIndex == NULL_INDEX)
+            {
+                it->tailOrderIndex = prevIndex;
+                orderPool[prevIndex].nextOrderIndex = NULL_INDEX;
+            }
+            else
+            {
+                orderPool[prevIndex].nextOrderIndex = nextIndex;
+                orderPool[nextIndex].prevOrderIndex = prevIndex;
+            }
+        }
+
+        orderMap[orderId] = NULL_INDEX;
+        deallocateOrder(targetIndex);
     }
 };
 
@@ -214,49 +278,64 @@ int main()
     OrderBook book(numOrders);
     std::vector<Order> testOrders;
 
-    random_device rd;  // seed
-    mt19937 gen(rd()); // Mersenne Twister RNG
-    uniform_int_distribution<> priceDist(1000, 1500);
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> bidPriceDist(1000, 1600); // Low prices
+    uniform_int_distribution<> askPriceDist(1500, 2000); // High prices
     uniform_int_distribution<> quantityDist(100, 150);
 
     for (int i = 0; i < numOrders; i++)
     {
         Order newOrder;
-        uint64_t randomPrice = priceDist(gen);
+        newOrder.quantity = quantityDist(gen);
+        newOrder.orderId = i;
+        newOrder.timestamp = i;
 
-        if ((randomPrice >> 1) & 1)
+        // Alternate Buys and Sells so we fill both sides of the book
+        if (i % 2 == 0)
         {
             newOrder.side = Side::Buy;
+            newOrder.price = bidPriceDist(gen);
         }
         else
         {
             newOrder.side = Side::Sell;
+            newOrder.price = askPriceDist(gen);
         }
 
-        newOrder.price = randomPrice;
-        newOrder.quantity = quantityDist(gen);
-        newOrder.orderId = i;
-        newOrder.timestamp = i;
         testOrders.push_back(newOrder);
     }
 
-    auto start = std::chrono::high_resolution_clock::now();
-
+    // --- 1. BENCHMARK PROCESS ORDER ---
+    auto startInsert = std::chrono::high_resolution_clock::now();
     for (Order &order : testOrders)
     {
         book.processOrder(order);
     }
+    auto endInsert = std::chrono::high_resolution_clock::now();
+    auto insertMicrosec = std::chrono::duration_cast<std::chrono::microseconds>(endInsert - startInsert).count();
 
-    auto end = std::chrono::high_resolution_clock::now();
-    auto totalMicrosec = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    // --- 2. BENCHMARK CANCEL ORDER (Cancel all odd IDs) ---
+    auto startCancel = std::chrono::high_resolution_clock::now();
+    for (Order &order : testOrders)
+    {
+        if (order.orderId % 2 != 0)
+        {
+            book.cancelOrder(order.orderId);
+        }
+    }
+    auto endCancel = std::chrono::high_resolution_clock::now();
+    auto cancelMicrosec = std::chrono::duration_cast<std::chrono::microseconds>(endCancel - startCancel).count();
 
-    double avgLatency = static_cast<double>(totalMicrosec) / numOrders;
-
+    // --- 3. PRINT STATS ---
     std::cout << "-----------------------------------\n";
-    std::cout << "Processed:       " << numOrders << " orders\n";
-    std::cout << "Total Time:      " << totalMicrosec / 1000.0 << " milliseconds\n";
-    std::cout << "Average Latency: " << avgLatency << " microseconds per order\n";
-    std::cout << "Throughput:      " << (numOrders / (totalMicrosec / 1000000.0)) << " orders per second\n";
+    std::cout << "INSERTION (1,000,000 Orders)\n";
+    std::cout << "Total Time:      " << insertMicrosec / 1000.0 << " ms\n";
+    std::cout << "Latency:         " << static_cast<double>(insertMicrosec) / numOrders << " us/order\n";
+    std::cout << "-----------------------------------\n";
+    std::cout << "CANCELLATION (500,000 Orders)\n";
+    std::cout << "Total Time:      " << cancelMicrosec / 1000.0 << " ms\n";
+    std::cout << "Latency:         " << static_cast<double>(cancelMicrosec) / (numOrders / 2) << " us/order\n";
     std::cout << "-----------------------------------\n";
 
     return 0;
